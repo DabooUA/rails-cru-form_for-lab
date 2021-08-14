40bc88491d22e002f07168409bed4a367ba9f4cc
DabooUA <sbardysh@tutanota.com>
2021-08-14 15:34:19 -0400

    commit tree 1c14475f863c11308606a724bb4b00536f5c55c2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:new, :show, :edit, :update, :create, :index]
+  resources :genres, only: [:new, :show, :edit, :update, :create]
+  resources :artists, only: [:new, :show, :edit, :update, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
